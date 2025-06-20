c69536b1c8c1501ed7ef3b5b70d337837103f428
ldladdd <132986646+ldl2714@users.noreply.github.com>
2025-06-20 13:32:46 +0800

    feat：首页 静态结构

--- app.vue
+++ app.vue
@@ -8,20 +8,17 @@ useSeoMeta({
 </script>
 
 <template>
-  <!-- 基于文件的路由系统 -->
-
-  <!-- 首页路由 index/index.vue -->
-  <NuxtLink to="/">首页</NuxtLink>
-  <!-- 视频页路由 video/index.vue -->
-  <NuxtLink to="/video">视频页</NuxtLink>
-  <!-- 页面路由 -->
   <NuxtPage />
 </template>
 
 
-<style>
+<style lang="scss">
+@use "@/assets/styles/base.scss" as *;
+@use "@/assets/styles/iconfont.scss" as *;
+
 :root {
   /* css 变量设置组件库主题 */
-  --van-primary-color: #FB7299 !important; /* 主要颜色 */
+  --van-primary-color: #2d161c !important;
+  /* 主要颜色 */
 }
 </style>
