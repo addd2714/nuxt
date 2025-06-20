1bacc4d81e35cc9a8b815b9c9535c95842e52849
ldladdd <132986646+ldl2714@users.noreply.github.com>
2025-06-20 10:51:01 +0800

    feat: 基于文件的路由系统

---
+++ app.vue
@@ -0,0 +1,10 @@
+<template>
+  <!-- 基于文件的路由系统 -->
+
+  <!-- 首页路由 index/index.vue -->
+  <NuxtLink to="/">首页</NuxtLink>
+  <!-- 视频页路由 video/index.vue -->
+  <NuxtLink to="/video">视频页</NuxtLink>
+  <!-- 页面路由 -->
+  <NuxtPage />
+</template>
---
+++ pages/video/index.vue
@@ -0,0 +1,13 @@
+<template>
+    <div>
+        视频页
+    </div>
+</template>
+
+<script setup lang="ts">
+
+</script>
+
+<style scoped>
+
+</style>
